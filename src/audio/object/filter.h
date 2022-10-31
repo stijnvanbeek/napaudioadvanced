2f2f10771f7bde5cbfe6652d49ae747e1143a54e
Stijn van Beek <stijn.v.beek@gmail.com>
2022-10-31 14:18:00 +0100

    mozilla public license 2.0

--- src/audio/object/filter.h
+++ src/audio/object/filter.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #pragma once
 
 // Audio includes
@@ -31,8 +35,6 @@ namespace nap
         private:
             bool initNode(int channel, FilterNode& node, utility::ErrorState& errorState) override;
         };
-
-
         
     }
     
