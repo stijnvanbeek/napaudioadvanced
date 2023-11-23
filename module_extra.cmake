285b940c9c6b1dd47e3043017e62e6dfc29e6583
Stijn van Beek <stijnvanbeek@users.noreply.github.com>
2023-11-23 18:38:40 +0100

    upgrade to nap 0.7 with napportaudio module

---
+++ module_extra.cmake
@@ -0,0 +1,18 @@
+# Add sources to target
+if(NAP_BUILD_CONTEXT MATCHES "source")
+    if (NAP_AUDIOFILE_SUPPORT)
+        # Add compile definition to enable audio file support
+        target_compile_definitions(${PROJECT_NAME} PRIVATE NAP_AUDIOFILE_SUPPORT)
+    else()
+        # Filter out sources for audio file functionality
+        set(AUDIO_FILE_SUPPORT_FILTER ".*audiofile.*")
+    endif()
+
+    add_source_dir("core" "src/audio/core")
+    add_source_dir("node" "src/audio/node" ${AUDIO_FILE_SUPPORT_FILTER})
+    add_source_dir("object" "src/audio/object" ${AUDIO_FILE_SUPPORT_FILTER})
+    add_source_dir("component" "src/audio/component")
+    add_source_dir("service" "src/audio/service")
+    add_source_dir("resource" "src/audio/resource" ${AUDIO_FILE_SUPPORT_FILTER})
+    add_source_dir("utility" "src/audio/utility")
+endif()
